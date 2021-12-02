4d04d36e756968b588fba397a8643a9310ceb6f6
cybergoat9152 <arthurtbarros@outlook.com>
2021-12-02 20:10:36 -0300

    CODEFIX -> adding '/' to endpoint

--- animehub/lib/globals/variables.dart
+++ animehub/lib/globals/variables.dart
@@ -1,5 +1,5 @@
 String url =
-    'https://mr9ezzzhu1.execute-api.sa-east-1.amazonaws.com/dev/animehub';
+    'https://mr9ezzzhu1.execute-api.sa-east-1.amazonaws.com/dev/animehub/';
 String logo =
     'https://cdn.discordapp.com/attachments/822141817520652299/907415173944463420/unknown.png';
 String logoMenu =
